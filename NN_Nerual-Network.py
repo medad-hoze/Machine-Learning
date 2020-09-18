4ec1b654c84aa08e8a0982bc3cd748c2eec2d17c
meidad hoze <48644044+medad-hoze@users.noreply.github.com>
2020-09-18 17:19:25 +0300

    Update NN_Nerual-Network.py

--- NN_Nerual-Network.py
+++ NN_Nerual-Network.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 
@@ -21,10 +20,10 @@ def relu_derivative(x):
 class Nerual_Network():
     
     def __init__(self,x,y,epoch):
-        self.x        = x
-        self.y        = y
-        self.shapeX   = x.shape
-        self.shapeY   = y.shape
+        self.x        = np.array(x)
+        self.y        = np.array(y)
+        self.shapeX   = self.x.shape
+        self.shapeY   = self.y.shape
         self.epoch    = epoch
         self.Pred     = []
 
@@ -70,76 +69,65 @@ class Nerual_Network():
     
     def Fit(self):
         
-        w0,b0  = self.Weights_Bias((self.shapeX[1],4))
-        w1,b1  = self.Weights_Bias((4,8))
-        w2,b2  = self.Weights_Bias((8,4))
-        w3,b3  = self.Weights_Bias((4,1))
+        self.w0,self.b0  = self.Weights_Bias((self.shapeX[1],4))
+        self.w1,self.b1  = self.Weights_Bias((4,8))
+        self.w2,self.b2  = self.Weights_Bias((8,4))
+        self.w3,self.b3  = self.Weights_Bias((4,1))
         
 
         for i in range(self.epoch):
             
-            l1 = self.Build_NetWork(x,w0,b0)
-            l2 = self.Build_NetWork(l1,w1,b1)
-            l3 = self.Build_NetWork(l2,w2,b2)
-            l4 = self.Build_NetWork(l3,w3,b3)
+            l1 = self.Build_NetWork(x,self.w0,self.b0)
+            l2 = self.Build_NetWork(l1,self.w1,self.b1)
+            l3 = self.Build_NetWork(l2,self.w2,self.b2)
+            l4 = self.Build_NetWork(l3,self.w3,self.b3)
             
 
             l4_error,l4_delta = self.ReValue_Last_Error_Delta(y,l4)
             
-            l3_error,l3_delta = self.ReValue_Error_Delta(l3,l4_delta,w3)
-            l2_error,l2_delta = self.ReValue_Error_Delta(l2,l3_delta,w2)
-            l1_error,l1_delta = self.ReValue_Error_Delta(l1,l2_delta,w1)
+            l3_error,l3_delta = self.ReValue_Error_Delta(l3,l4_delta,self.w3)
+            l2_error,l2_delta = self.ReValue_Error_Delta(l2,l3_delta,self.w2)
+            l1_error,l1_delta = self.ReValue_Error_Delta(l1,l2_delta,self.w1)
     
 
-            w0,b0 = self.ReValue_Weight_Bias     (x,l1_delta,w0,b0)
-            w1,b1 = self.ReValue_Weight_Bias     (l1,l2_delta,w1,b1)
-            w2,b2 = self.ReValue_Weight_Bias     (l2,l3_delta,w2,b2)
-            w3,b3 = self.ReValue_Last_Weight_Bias(l3,l4_delta,w3,b3)
+            self.w0,self.b0 = self.ReValue_Weight_Bias     (self.x,l1_delta,self.w0,self.b0)
+            self.w1,self.b1 = self.ReValue_Weight_Bias     (l1,l2_delta,self.w1,self.b1)
+            self.w2,self.b2 = self.ReValue_Weight_Bias     (l2,l3_delta,self.w2,self.b2)
+            self.w3,self.b3 = self.ReValue_Last_Weight_Bias(l3,l4_delta,self.w3,self.b3)
             
             
             self.Loss(l4_error,i)
-        
-        self.Pred.append([[w0,b0],[w1,b1],[w2,b2],[w3,b3]])
             
         return l4_delta
     
-    def Predict(self,x):
+    def Predict(self,data):
         
-            w0,b0 = self.Pred[0]
-            w1,b1 = self.Pred[1]
-            w2,b2 = self.Pred[2]
-            w3,b3 = self.Pred[3]
-        
-            l1 = self.Build_NetWork(x,w0,b0)
-            l2 = self.Build_NetWork(l1,w1,b1)
-            l3 = self.Build_NetWork(l2,w2,b2)
-            l4 = self.Build_NetWork(l3,w3,b3)
-            
-            l4_error,l4_delta = self.ReValue_Last_Error_Delta(y,l4)
+            l1 = self.Build_NetWork(data,self.w0,self.b0)
+            l2 = self.Build_NetWork(l1,self.w1,self.b1)
+            l3 = self.Build_NetWork(l2,self.w2,self.b2)
+            l4 = self.Build_NetWork(l3,self.w3,self.b3)
             
             return l4
     
     
-    
-    
+# # # # # # # # # #  TEST CODE # # # # # # # # # # # 
+            
+# Train NetWork
+            
 np.random.seed(1)
 
-x = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1],[1,1,1]])
-y = np.array([0,0,1,1,1])
+x = [[0,0,1],[0,1,1],[1,0,1],[1,1,1],[1,1,1]]
+y = [0,0,1,1,1]
 
         
-nn1 = Nerual_Network(x,y,1000000)
+nn1 = Nerual_Network(x,y,10000)
 
 nn1.Fit()
 
 
-x = np.array([[1,1,1],[1,0,1],[1,0,1],[0,1,1],[0,0,1]])
+# Predict NetWork
 
 
-print (nn1.Predict(x))
+data = [[0,0,1],[0,1,1],[1,0,1],[1,1,1],[1,1,1],[0,0,0],[0,0,1],[1,0,0],[0,1,0],[1,1,0]]
 
-
-
-        
-
-
+print (nn1.Predict(data))
